2f34fb3fd67f280eb479d616fff979d6f9e8b500
Jay <jay.young@gmail.com>
2016-11-23 09:00:46 +0000

    getting there

--- src/filter.cpp
+++ src/filter.cpp
@@ -20,7 +20,12 @@
 #include <pcl/filters/statistical_outlier_removal.h>
 #include <sensor_msgs/Image.h>
 #include "bham_seg_filter/bham_seg.h"
+#include "segmentation_srv_definitions/segment.h"
+
+
 #include <pcl/io/pcd_io.h>
+
+
 #include <geometry_msgs/PointStamped.h>
 #include <tf/transform_listener.h>
 #include <pcl/features/normal_3d.h>
@@ -33,52 +38,31 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/range_image/range_image.h>
-#include <pcl/segmentation/conditional_euclidean_clustering.h>
+
+
+
 using namespace std;
 using namespace sensor_msgs;
 #include <pcl/filters/passthrough.h>
-
-typedef pcl::PointXYZ PointT;
-
-typedef pcl::PointXYZI PointTypeIO;
-typedef pcl::PointXYZINormal PointTypeFull;
-#define ANGLE_MAX_DIFF (M_PI / 4)
-
-bool enforceNormals (const PointTypeFull& point_a, const PointTypeFull& point_b, float squared_distance)
-{
-  Eigen::Vector3f point_a_normal = point_a.getVector3fMap();
-  Eigen::Vector3f point_b_normal = point_b.getVector3fMap();
-  float angle = point_a_normal.dot(point_b_normal);
-  std::cout << angle << std::endl;
-  return true;
-
-
-  if (fabs (point_a.intensity - point_b.intensity) < 5.0f)
-    return (true);
-  if (fabs (point_a_normal.dot (point_b_normal)) < 0.05)
-    return (true);
-  return (false);
-}
-
 
 sensor_msgs::PointCloud2 ransac_filter(sensor_msgs::PointCloud2 input_cloud)
 {
   ROS_INFO("Doing RANSAC filter");
   pcl::PCDWriter writer;
-ros::NodeHandle nh_;
   pcl::PCLPointCloud2 pcl_pc2;
 
   pcl_conversions::toPCL(input_cloud,pcl_pc2);
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
-
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr col_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  //pcl::PointCloud<pcl::PointXYZRGB>::Ptr col_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
   pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
 
-  pcl::io::loadPCDFile<pcl::PointXYZ> ("2.pcd", *temp_cloud);
+//  pcl::io::loadPCDFile<pcl::PointXYZRGB> ("2.pcd", *temp_cloud);
+//  pcl::io::loadPCDFile<pcl::PointXYZRGB> ("3.pcd", *col_cloud);
 
+//    writer.write<pcl::PointXYZRGB> ("cc.pcd", *col_cloud, false);
 //  pcl::VoxelGrid<pcl::PointXYZ> sor;
 //  sor.setInputCloud (temp_cloud);
 //  sor.setLeafSize (0.1f, 0.1f, 0.1f);
@@ -100,9 +84,9 @@ ros::NodeHandle nh_;
   pcl::ExtractIndices<pcl::Normal> extract_normals;
 
   pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-  pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT> ());
+  pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB> ());
 
-  pcl::NormalEstimation<PointT, pcl::Normal> ne;
+  pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
   ne.setSearchMethod (tree);
   ne.setInputCloud (temp_cloud);
   ne.setKSearch (75);
@@ -135,13 +119,13 @@ ros::NodeHandle nh_;
     writer.write<pcl::PointXYZ> ("up.pcd", *up_cloud, false);
 */
 
-  pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg;
+  pcl::SACSegmentationFromNormals<pcl::PointXYZRGB, pcl::Normal> seg;
   // Optional
   seg.setOptimizeCoefficients (true);
   // Mandatory
   seg.setModelType (pcl::SACMODEL_NORMAL_PLANE );
   seg.setMethodType (pcl::SAC_RANSAC);
-  seg.setDistanceThreshold (0.018);
+  seg.setDistanceThreshold (0.015);
   seg.setInputNormals (cloud_normals);
   seg.setNormalDistanceWeight (0.001);
   seg.setMaxIterations(1000);
@@ -157,18 +141,33 @@ ros::NodeHandle nh_;
 
   std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
   // do ransac stuff
-  pcl::ExtractIndices<pcl::PointXYZ> extract;
+  pcl::ExtractIndices<pcl::PointXYZRGB> extract;
   extract.setInputCloud (temp_cloud);
   extract.setIndices (inliers);
-  extract.setNegative (false);
+  extract.setNegative (true);
   extract.filter (*filtered_cloud);
 
-  sensor_msgs::Image image_;
-  sensor_msgs::PointCloud2 pc;
-  writer.write<pcl::PointXYZ> ("filtered.pcd", *filtered_cloud, false);
+//  sensor_msgs::Image image_;
+//  sensor_msgs::PointCloud2 pc;
+  writer.write<pcl::PointXYZRGB> ("filtered.pcd", *filtered_cloud, false);
+  sensor_msgs::PointCloud2 output_cloud;
+  pcl::toROSMsg(*filtered_cloud,output_cloud);
+
+
+/*
+    ros::Publisher image_pub_ = nh_.advertise<sensor_msgs::PointCloud2> ("/willthiswork", 30);
+    ros::Rate r(10); // 10 hz
+    int i = 0;
+    while(i < 10) {
+      i++;
+      ROS_INFO("Publishing");
+      image_pub_.publish (output_cloud);
+      r.sleep();
+    }
+    */
 /*
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_col_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
@@ -192,7 +191,7 @@ ros::NodeHandle nh_;
   }
 */
 
-  return input_cloud;
+  return output_cloud;
 }
 
 bool segment_cb(
@@ -202,6 +201,7 @@ bham_seg_filter::bham_seg::Response &res)
   ROS_INFO("Received service call");
   sensor_msgs::PointCloud2 filtered_cloud = ransac_filter(req.cloud);
 
+  //ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
 
 
   return true;
@@ -210,16 +210,14 @@ bham_seg_filter::bham_seg::Response &res)
 int main (int argc, char** argv)
 {
   ros::init (argc, argv, "bham_filtered_segmentation");
+  ros::NodeHandle node;
+
   //ros::NodeHandle node;
 
   ROS_INFO("Setting up services");
-  //ros::ServiceServer conversion_service = node.advertiseService("/bham_filtered_segmentation/segment", segment_cb);
+  ros::ServiceServer conversion_service = node.advertiseService("/bham_filtered_segmentation/segment", segment_cb);
   ROS_INFO("Done, ready to go");
 
-  sensor_msgs::PointCloud2 input_cloud;
-  ransac_filter(input_cloud);
-
-  ROS_INFO("Spinning");
-  //ros::spin();
+  ros::spin();
   return 0;
 }
